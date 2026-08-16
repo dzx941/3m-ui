e257eecc66555b0122d3341afb1d1f8b5516ea7b
風雪 <dzx1283@126.com>
2026-08-16 09:35:03 +0800

    Add files via upload

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"embed"
+	"log"
+	"os"
+
+	"github.com/dzx941/3m-ui/backend/internal/app"
+)
+
+//go:embed web/dist/*
+var frontendFiles embed.FS
+
+func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "version") {
+		println(versionString())
+		return
+	}
+	if err := app.Run(frontendFiles); err != nil {
+		log.Fatal(err)
+	}
+}
